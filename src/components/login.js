88b2faf80c90f9a87ddbf34330196430af7fdec5
Fawas Ajani <G00413222@atu.ie>
2025-03-08 15:14:53 +0000

    password

--- src/components/login.js
+++ src/components/login.js
@@ -34,10 +34,27 @@ function Login() {
 
         setLoading(false);
     };
-    
+
     return (
         <div className="login-container">
             <h2>Login to CyberCafeX</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label>Email:</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
