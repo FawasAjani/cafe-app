f6d5a31bd212a7d67e448f1cb6daa837070b94fa
Fawas Ajani <G00413222@atu.ie>
2025-03-08 15:04:33 +0000

    function page

--- src/components/login.js
+++ src/components/login.js
@@ -2,4 +2,4 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../App.css"; // Import your styling
+import "../App.css"; 
