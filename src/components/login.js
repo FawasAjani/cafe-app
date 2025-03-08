7e2e59b2321289863b71a0511b8f69481fecbe91
Fawas Ajani <G00413222@atu.ie>
2025-03-08 15:03:03 +0000

    login page

---
+++ src/components/login.js
@@ -0,0 +1,5 @@
+// FawasAjani-G00413222
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../App.css"; // Import your styling
