a86b63186ebb53b00de2823d05c64e964c5aaf3c
Fawas Ajani <G00413222@atu.ie>
2025-03-10 14:13:06 +0000

    Sweet potato image added

--- src/components/order.js
+++ src/components/order.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 //images
 import garlicpittaImage from '../images/garlic_pitta.jpg';
 import olivesImage from '../images/olives.jpg';
+import SweetpotatoImage from '../images/sweet-potato-wedges.jpg';
 import "bootstrap/dist/css/bootstrap.min.css";
 //Food iteams
 const foodItems = [
     { id: 1, category: "Starters", name: "Cheesy Garlic Pitta", price: 6.25, image: garlicpittaImage },
     { id: 2, category: "Starters", name: "Halloumi Sticks & Dip", price: 5.25, image: olivesImage },
-    { id: 3, category: "Main Dishes", name: "Classic Burger", price: 9.99, image: "burger.jpg" },
-    { id: 4, category: "Main Dishes", name: "Grilled Chicken", price: 12.99, image: "grilled_chicken.jpg" },
-    { id: 5, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
-    { id: 6, category: "Desserts", name: "Ice Cream Sundae", price: 5.49, image: "ice_cream.jpg" },
+    { id: 3, category: "Starters", name: "Sweet potato", price: 8.00, image: SweetpotatoImage  },
+    { id: 4, category: "Main Dishes", name: "Classic Burger", price: 9.99, image: "burger.jpg" },
+    { id: 6, category: "Main Dishes", name: "Grilled Chicken", price: 12.99, image: "grilled_chicken.jpg" },
+    { id: 7, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
+    { id: 8, category: "Desserts", name: "Ice Cream Sundae", price: 5.49, image: "ice_cream.jpg" },
 ];
 
 function Order({ basket, setBasket }) {
