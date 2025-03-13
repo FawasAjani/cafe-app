cbf09d913e8a709396180c3e4a142d8bee55af8c
Fawas Ajani <G00413222@atu.ie>
2025-03-13 14:49:03 +0000

    added chicken wings to the order page

--- src/components/order.js
+++ src/components/order.js
@@ -1,20 +1,27 @@
-import { useState } from "react";
+
 //images
 import garlicpittaImage from '../images/garlic_pitta.jpg';
 import olivesImage from '../images/olives.jpg';
 import SweetpotatoImage from '../images/sweet-potato-wedges.jpg';
-import HalloumiImage from '../images/halloumi_sticks.jpg';
+import HalloumiImage from '../images/sweet-potato-wedges.jpg';
+import almondsImage from '../images/almonds.jpg';
+import maccheeseImage from '../images/Ajani mac&cheese.jpg';
+import burgerImage from '../images/burger.jpg';
+import chickenwingsImage from '../images/chickenwings.jpg';
 import "bootstrap/dist/css/bootstrap.min.css";
 //Food iteams
 const foodItems = [
     { id: 1, category: "Starters", name: "Cheesy Garlic Pitta", price: 6.25, image: garlicpittaImage },
     { id: 2, category: "Starters", name: "olives", price: 5.25, image: olivesImage },
     { id: 3, category: "Starters", name: "Sweet potato", price: 8.00, image: SweetpotatoImage  },
-    { id: 4, category: "Starters", name: "Halloumi Sticks & Dip", price: 8.00, image: HalloumiImageImage  },
-    { id: 5, category: "Main Dishes", name: "Classic Burger", price: 9.99, image: "burger.jpg" },
-    { id: 6, category: "Main Dishes", name: "Grilled Chicken", price: 12.99, image: "grilled_chicken.jpg" },
-    { id: 7, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
-    { id: 8, category: "Desserts", name: "Ice Cream Sundae", price: 5.49, image: "ice_cream.jpg" },
+    { id: 4, category: "Starters", name: "Halloumi Sticks & Dip", price: 9.60, image: HalloumiImage  },
+    { id: 5, category: "Starters", name: "Almonds", price: 1.90, image: almondsImage  },
+    { id: 6, category: "Starters", name: "Ajani mac&cheese", price: 12.00, image: maccheeseImage  },
+    { id: 7, category: "Main Dishes" ,name: "Classic Burger", price: 20.99, image: burgerImage },
+    { id: 8, category: "Main Dishes", name: "3 chicken wings", price: 24.00, image: chickenwingsImage  },
+    
+    { id: 10, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
+    { id: 11, category: "Desserts", name: "Ice Cream Sundae", price: 5.49, image: "ice_cream.jpg" },
 ];
 
 function Order({ basket, setBasket }) {
