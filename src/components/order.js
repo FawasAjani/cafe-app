ca5d1c5785e111acce095cf9bfdc7b727b31ee50
Fawas Ajani <G00413222@atu.ie>
2025-03-14 15:00:12 +0000

    sides page added to app

--- src/components/order.js
+++ src/components/order.js
@@ -1,34 +1,41 @@
-
-//import images
+// import images
 import garlicpittaImage from '../images/garlic_pitta.jpg';
 import olivesImage from '../images/olives.jpg';
 import SweetpotatoImage from '../images/sweet-potato-wedges.jpg';
-import HalloumiImage from '../images/sweet-potato-wedges.jpg';
+import HalloumiImage from '../images/halloumi-sticks.jpg';
 import almondsImage from '../images/almonds.jpg';
 import maccheeseImage from '../images/Ajani mac&cheese.jpg';
 import burgerImage from '../images/burger.jpg';
 import chickenwingsImage from '../images/chickenwings.jpg';
 import chickenliverportugueserollImage from '../images/chicken-liverportugueseroll.jpg';
 import chickenbreastfilletImage from '../images/chicken-breast-fillet.jpg';
-import doublechickenwrapImage from '../images/double-chicken-wrap.jpg'; 
+import doublechickenwrapImage from '../images/double-chicken-wrap.jpg';
+import veganspicyriceImage from '../images/spicy-rice-bowl.jpg';
+import fullyloadedchipsImage from '../images/fully-loaded-chips.jpg';
+
 import "bootstrap/dist/css/bootstrap.min.css";
-//Food iteams
+
+// Food items
 const foodItems = [
     { id: 1, category: "Starters", name: "Cheesy Garlic Pitta", price: 6.25, image: garlicpittaImage },
-    { id: 2, category: "Starters", name: "olives", price: 5.25, image: olivesImage },
-    { id: 3, category: "Starters", name: "Sweet potato", price: 8.00, image: SweetpotatoImage  },
-    { id: 4, category: "Starters", name: "Halloumi Sticks & Dip", price: 9.60, image: HalloumiImage  },
-    { id: 5, category: "Starters", name: "Almonds", price: 1.90, image: almondsImage  },
-    { id: 6, category: "Starters", name: "Ajani mac&cheese", price: 12.00, image: maccheeseImage  },
-    { id: 7, category: "Main Dishes" ,name: "Classic Burger", price: 20.99, image: burgerImage },
-    { id: 8, category: "Main Dishes", name: "3 chicken wings", price: 24.00, image: chickenwingsImage  },
-    { id: 9, category: "Main Dishes", name: "Chicken Livers & Portuguese Roll", price: 30.00, image:chickenliverportugueserollImage },
-    { id: 10, category: "Main Dishes", name: "chickenbreast-fillet", price: 24.00, image: chickenbreastfilletImage  },
-    { id: 11, category: "Main Dishes", name: "double-chicken", price: 19.00, image:doublechickenwrapImage },
-    { id: 12, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
-    { id: 13, category: "Desserts", name: "Chocolate Brownie", price: 4.99, image: "brownie.jpg" },
-    { id: 14, category: "Desserts", name: "Ice Cream Sundae", price: 5.49, image: "ice_cream.jpg" },
+    { id: 2, category: "Starters", name: "Olives", price: 5.25, image: olivesImage },
+    { id: 3, category: "Starters", name: "Sweet Potato", price: 8.00, image: SweetpotatoImage },
+    { id: 4, category: "Starters", name: "Halloumi Sticks & Dip", price: 9.60, image: HalloumiImage },
+    { id: 5, category: "Starters", name: "Almonds", price: 1.90, image: almondsImage },
+    { id: 6, category: "Starters", name: "Ajani Mac & Cheese", price: 12.00, image: maccheeseImage },
+    { id: 7, category: "Main Dishes", name: "Classic Burger", price: 20.99, image: burgerImage },
+    { id: 8, category: "Main Dishes", name: "3 Chicken Wings", price: 24.00, image: chickenwingsImage },
+    { id: 9, category: "Main Dishes", name: "Chicken Livers & Portuguese Roll", price: 30.00, image: chickenliverportugueserollImage },
+    { id: 10, category: "Main Dishes", name: "Chicken Breast Fillet", price: 24.00, image: chickenbreastfilletImage },
+    { id: 11, category: "Main Dishes", name: "Double Chicken Wrap", price: 19.00, image: doublechickenwrapImage },
+    { id: 12, category: "Main Dishes", name: "Vegan Spicy Rice", price: 40.00, image: veganspicyriceImage },
+    { id: 13, category: "Sides", name: "Fully Loaded Chips", price: 40.00, image: fullyloadedchipsImage },
+   
+
 ];
+
+
+
 
 function Order({ basket, setBasket }) {
     const addToBasket = (item) => {
@@ -38,7 +45,7 @@ function Order({ basket, setBasket }) {
     return (
         <div className="container mt-4">
             <h2>Order Food</h2>
-            {['Starters', 'Main Dishes', 'Desserts'].map((category) => (
+            {['Starters', 'Main Dishes','Sides', 'Desserts'].map((category) => (
                 <div key={category} className="mb-4">
                     <h3>{category}</h3>
                     <div className="row">
