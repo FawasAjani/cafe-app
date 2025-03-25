5b1975702d3f91b05f29c04771963798d3a5b445
Fawas Ajani <G00413222@atu.ie>
2025-03-25 16:36:38 +0000

    rating

--- src/components/review.js
+++ src/components/review.js
@@ -25,7 +25,7 @@ function Review() {
                 setMessage('Review submitted successfully!');
                 setName('');//customer name
                 setComment('');//comment
-                setRating(5);
+                setRating(10);//rating
                 fetchReviews(); // Refresh the reviews
             })
             .catch(error => setMessage('Error submitting review: ' + error.message));
