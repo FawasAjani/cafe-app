9c9f81d868b6471dc57b67ba0bb45267b0db1ad1
Fawas Ajani <G00413222@atu.ie>
2025-04-19 16:39:58 +0100

    Functional component Payment

--- src/components/payment.js
+++ src/components/payment.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+// Functional component Payment 
 function Payment({ basket, clearBasket }) {
     const [cardNumber, setCardNumber] = useState("");
     const [expiry, setExpiry] = useState("");
